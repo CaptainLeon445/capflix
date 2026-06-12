fb6674b68eb37b798c63e2c674830d2bf6f74d41
CaptainLeon445 <christopherleon237@gmail.com>
2026-06-12 17:27:23 +0100

    feat: wire search bar navigation and add placeholder prop

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,7 +2,9 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Image, ScrollView, View } from "react-native";
 import SearchBar from "../components/searchBar";
+import { useRouter } from "expo-router";
 export default function Index() {
+  const router=useRouter()
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
@@ -10,7 +12,10 @@ export default function Index() {
         minHeight: "100%", paddingBottom: 10
       }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <SearchBar />
+        <SearchBar
+           onPress={() => router.push("/search")}
+           placeholder="Search for a movie"
+           />
       </ScrollView>
     </View>
   );
